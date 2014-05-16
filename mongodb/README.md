e79a3563ec28d90413eeeeb59e6061fc7e6fd378
Tran The Trong <trongtt@gmail.com>
2014-05-16 11:44:56 +0700

    Add MongoDB examination

---
+++ mongodb/README.md
@@ -0,0 +1,5 @@
+The purpose of this POC is to provide a MongoDB based implementation for eXo Organization Service.
+
+Specification:
+
+- Build on top of eXo Core 2.5.0-GA
---
+++ mongodb/src/main/java/t3/pocs/mongodb/organization/MongoOrganizationServiceImpl.java
@@ -0,0 +1,9 @@
+package t3.pocs.mongodb.organization;
+
+import org.exoplatform.container.component.ComponentRequestLifecycle;
+import org.exoplatform.services.organization.BaseOrganizationService;
+import org.picocontainer.Startable;
+
+public class MongoOrganizationServiceImpl extends BaseOrganizationService implements Startable, ComponentRequestLifecycle {
+
+}
